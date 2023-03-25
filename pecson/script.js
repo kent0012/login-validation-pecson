d3c785594087489b5965f0d12e09abad8f647cf8
kent0012 <120572246+kent0012@users.noreply.github.com>
2023-03-25 09:54:22 +0800

    Add files via upload

---
+++ pecson/script.js
@@ -0,0 +1,140 @@
+//Email
+let emailInput = document.getElementById("email");
+let emailError = document.getElementById("email-error");
+let emptyEmailError = document.getElementById("empty-email");
+
+
+//First Name
+let firstNameInput = document.getElementById("first-name-input");
+let firstNameError = document.getElementById("first-name-error");
+let emptyFirstNameError = document.getElementById("empty-first-name");
+
+//Last name
+let lastNameInput = document.getElementById("last-name-input");
+let lastNameError = document.getElementById("last-name-error");
+let emptyLastNameError = document.getElementById("empty-last-name");
+
+//Phone
+let phoneInput = document.getElementById("phone");
+let phoneError = document.getElementById("phone-error");
+let emptyPhoneError = document.getElementById("empty-phone");
+
+//jobposition
+let jobpositionInput = document.getElementById("jobposition");
+let jobpositionError = document.getElementById("jobposition-error");
+let emptyjobpositionError = document.getElementById("empty-jobposition");
+
+//Submit
+let submitButton = document.getElementById("submit-button");
+
+//Text verification (if input contains only text)
+const textVerify = (text) => {
+  const regex = /^[a-zA-Z]{3,}$/;
+  return regex.test(text);
+};
+
+//Phone number verification
+const phoneVerify = (number) => {
+  const regex = /^[0-9]{10}$/;
+  return regex.test(number);
+};
+
+//Email verification
+const emailVerify = (input) => {
+  const regex = /^[a-z0-9_]+@[a-z]{3,}\.[a-z\.]{3,}$/;
+  return regex.test(input);
+};
+
+//For empty input - accepts(input,empty error for that input and other errors)
+const emptyUpdate = (
+  inputReference,
+  emptyErrorReference,
+  otherErrorReference
+) => {
+  if (!inputReference.value) {
+    //input is null/empty
+    emptyErrorReference.classList.remove("hide");
+    otherErrorReference.classList.add("hide");
+    inputReference.classList.add("error");
+  } else {
+    //input has some content
+    emptyErrorReference.classList.add("hide");
+  }
+};
+
+//For error styling and displaying error message
+const errorUpdate = (inputReference, errorReference) => {
+  errorReference.classList.remove("hide");
+  inputReference.classList.remove("valid");
+  inputReference.classList.add("error");
+};
+
+//For no errors
+const validInput = (inputReference) => {
+  inputReference.classList.remove("error");
+  inputReference.classList.add("valid");
+};
+//Email
+emailInput.addEventListener("input", () => {
+  if (emailVerify(emailInput.value)) {
+    emailError.classList.add("hide");
+    validInput(emailInput);
+  } else {
+    errorUpdate(emailInput, emailError);
+    emptyUpdate(emailInput, emptyEmailError, emailError);
+  }
+});
+
+//First name
+firstNameInput.addEventListener("input", () => {
+  if (textVerify(firstNameInput.value)) {
+    //If verification returns true
+    firstNameError.classList.add("hide");
+    validInput(firstNameInput);
+  } else {
+    //for false
+    errorUpdate(firstNameInput, firstNameError);
+    //empty checker
+    emptyUpdate(firstNameInput, emptyFirstNameError, firstNameError);
+  }
+});
+
+//Last name
+lastNameInput.addEventListener("input", () => {
+  if (textVerify(lastNameInput.value)) {
+    lastNameError.classList.add("hide");
+    validInput(lastNameInput);
+  } else {
+    errorUpdate(lastNameInput, lastNameError);
+    emptyUpdate(lastNameInput, emptyLastNameError, lastNameError);
+  }
+});
+//Phone
+phoneInput.addEventListener("input", () => {
+  if (phoneVerify(phoneInput.value)) {
+    phoneError.classList.add("hide");
+    validInput(phoneInput);
+  } else {
+    errorUpdate(phoneInput, phoneError);
+    emptyUpdate(phoneInput, emptyPhoneError, phoneError);
+  }
+});
+//Jobposition
+JobpositionInput.addEventListener("input", () => {
+  if (phoneVerify(jobpositionInput.value)) {
+    jobpositionError.classList.add("hide");
+    validInput(jobpositionInput);
+  } else {
+    errorUpdate(jobpositionInput, jobpositionError);
+    emptyUpdate(jobpositionInput, emptyjobpositionError, jobpositionError);
+  }
+});
+
+//Submit button
+submitButton.addEventListener("click", () => {
+  if (validClasses.length == 6 && invalidClasses.length == 0) {
+    alert("Success");
+  } else {
+    alert("Error");
+  }
+});
